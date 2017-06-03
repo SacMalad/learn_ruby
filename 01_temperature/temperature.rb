199080e84d18bbdf300ed6a95520ef94cbc418ce
Justin <jusboyce@gmail.com>
2017-06-03 05:10:50 -0400

    Completed test 2, added code

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,16 @@
 #write your code here
+def ftoc(temp)
+  temp.to_f
+  temp -= 32
+  temp *= 5
+  temp /= 9
+  return temp
+end
+
+def ctof(temp)
+  temp.to_f
+  temp *= 9.0
+  temp /= 5.0
+  temp += 32.0
+  return temp
+end
