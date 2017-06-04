842a39e37b8fcf94732c5a340272a5aad15b54a2
Justin <jusboyce@gmail.com>
2017-06-04 04:20:39 -0400

    TODO: finish working on simon says crap.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,21 @@
 #write your code here
+  def echo(word)
+    word
+  end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, times = 2)
+  ([word] * times).join(" ")
+
+end
+
+def start_of_word(word, num)
+  word[0...num]
+end
+
+def first_word(word)
+  word.split(" ").first
+end
